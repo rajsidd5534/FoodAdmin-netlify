72407c9551612cac0032c373bc7a2911b7867dd0
Md Raj Siddiqui <rajsidd5534@gmail.com>
2025-09-07 04:42:35 +0530

    some change

--- src/pages/ListFood/ListFood.jsx
+++ src/pages/ListFood/ListFood.jsx
@@ -1,40 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { toast } from 'react-toastify';
-import { deleteFood, getFoodList } from '../../services/foodService';
-import './ListFood.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import "./ListFood.css";
+import { deleteFood, getFoodList } from "../../services/foodService";
 
 const ListFood = () => {
   const [list, setList] = useState([]);
-  const fetchList = async () => {  
-   try {
-  const data=  await getFoodList();
-   setList(data);
-   } catch (error) {
-    toast.error('Error while reading the foods.'); 
-   }
-    
-  }
+  const fetchList = async () => {
+    try {
+      const data = await getFoodList();
+      setList(data);
+    } catch (error) {
+      toast.error("Error while reading the foods.");
+    }
+  };
 
   const removeFood = async (foodId) => {
-   try {
-  const success = await deleteFood(foodId);
-  if (success) {
-    toast.success('food removed.');
-   await fetchList();
-  }else{
-   toast.error('Error occured while removing the food. ');
-  }
-   } catch (error) {
-    toast.error('Error occured while removing the food. ');
-   }
-  }
-   useEffect(() => {
-   fetchList();
-   }, []);
+    try {
+      const success = await deleteFood(foodId);
+      if (success) {
+        toast.success("Food removed.");
+        await fetchList();
+      } else {
+        toast.error("Error occred while removing the food.");
+      }
+    } catch (error) {
+      toast.error("Error occred while removing the food.");
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
   return (
     <div className="py-5 row justify-content-center">
       <div className="col-11 card">
-        <table className='table'>
+        <table className="table">
           <thead>
             <tr>
               <th>Image</th>
@@ -45,30 +46,29 @@ const ListFood = () => {
             </tr>
           </thead>
           <tbody>
-            {
-            list.map((item, index) =>{
+            {list.map((item, index) => {
               return (
-             <tr key={index}>
-              <td>
-                <img src={item.imageUrl} alt="" height={48} width={48} />
-              </td>
-              <td>{item.name}</td>
-              <td>{item.category}</td>  
-              <td>&#8377;{item.price}.00</td>
-              <td className='text-danger'>
-                <i className='bi bi-x-circle-fill' onClick={() => removeFood(item.id)}></i>
-              </td>
-             </tr>
-              )
-
-            })
-            }
+                <tr key={index}>
+                  <td>
+                    <img src={item.imageUrl} alt="" height={48} width={48} />
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.category}</td>
+                  <td>&#8377;{item.price}.00</td>
+                  <td className="text-danger">
+                    <i
+                      class="bi bi-trash-fill fs-4"
+                      onClick={() => removeFood(item.id)}
+                    ></i>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
-
-        </div>  
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ListFood;
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,72 +1,76 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { assets } from '../../assets/assets';
+import { fetchAllOrders, updateOrderStatus } from "../../services/orderService";
 
 const Orders = () => {
-   const [data , setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    const fetchOrders = async () => {
-      const response = await axios.get("https://fooapi-backend-production.up.railway.app/api/orders/all");
-      setData(response.data);
-    };
+  const fetchOrders = async () => {
+    try {
+      const response = await fetchAllOrders();
+      setData(response);
+    } catch (error) {
+      toast.error("Unable to display the orders. Please try again.");
+    }
+  };
 
-     const updateStatus = async (event, orderId) => {
-      const response =  await axios.patch(`https://fooapi-backend-production.up.railway.app/api/orders/status/${orderId}?status=${event.target.value}`);
-      if (response.status === 200) {
-       await fetchOrders(); 
-      }
-    };
+  const updateStatus = async (event, orderId) => {
+    const success = await updateOrderStatus(orderId, event.target.value);
+    if (success) await fetchOrders();
+  };
 
-      useEffect(() => {
-        fetchOrders();
-      }, []);
-    
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
   return (
-   <div className="container">
-    <div className="py-5 row justify-content-center">
+    <div className="container">
+      <div className="py-5 row justify-content-center">
         <div className="col-11 card">
-             <table className='table table-responsive'>
-                <tbody>
-                    {
-                    data.map((order , index ) => {
-                        return (
-                            <tr key={index}>
-                                <td>
-                                    <img src={assets.parcel} alt=""  height={48} width={48}/>
-                                </td>
-                                <td>
-                                     <div>
-                                        {order.orderedItems.map((item , index ) =>{
-                                    if(index === order.orderedItems.length - 1) {
-                                        return item.name + "x" + item.quantity;
-                                    } else{
-                                        return item.name + "x" + item.quantity + ", ";
-                                    }
-                                })}
-                                     </div>
-                                     <div>
-                                        {order.userAddress}
-                                     </div>
-                                </td>
-                                <td>&#8377;{order.amount}</td>
-                                <td>Items : {order.orderedItems.length}</td>
-                                <td>
-                                    <select className='form-control' onChange={(event) => updateStatus(event, order.id)} value={order.orderStatus}>
-                                        <option value="Food Preparing">Food Preparing</option>
-                                        <option value="out-for-delivery">Out for delivery</option>
-                                        <option value="delivered">Delivered</option>
-                                    </select>
-                                </td>
-                            </tr>
-                        )
-
-                    })
-                }
-                </tbody>
-             </table>
+          <table className="table table-responsive">
+            <tbody>
+              {data.map((order, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <img src={assets.parcel} alt="" height={48} width={48} />
+                    </td>
+                    <td>
+                      <div>
+                        {order.orderedItems.map((item, index) => {
+                          if (index === order.orderedItems.length - 1) {
+                            return item.name + " x " + item.quantity;
+                          } else {
+                            return item.name + " x " + item.quantity + ", ";
+                          }
+                        })}
+                      </div>
+                      <div>{order.userAddress}</div>
+                    </td>
+                    <td>&#x20B9;{order.amount.toFixed(2)}</td>
+                    <td>Items: {order.orderedItems.length}</td>
+                    <td>
+                      <select
+                        className="form-control"
+                        onChange={(event) => updateStatus(event, order.id)}
+                        value={order.orderStatus}
+                      >
+                        <option value="Food Preparing">Food Preparing</option>
+                        <option value="Out for delivery">
+                          Out for delivery
+                        </option>
+                        <option value="Delivered">Delivered</option>
+                      </select>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
+      </div>
     </div>
-   </div>
   );
 };
 
